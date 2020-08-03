37b4715fa4454395edc76ab05c343e1c8566a99f
Patrick Amsler <patrick.amsler@zuhlke.com>
2020-08-02 18:57:29 -0700

    doc added

--- main.tf
+++ main.tf
@@ -15,42 +15,42 @@ module "public_subnet_a" {
     source = "./modules/public_subnet"
     name = "tf-demo-public-subnet-a"
     vpc_id = module.tf_demo_vpc.vpc_id
-    subnet_cidr_block = "10.0.0.0/24" // 251
+    subnet_cidr_block = "10.0.0.0/24" // 251 public addresses
     owner = var.owner
     environment = var.environment
     az = "${var.region}a"
-    igw_id = module.tf_demo_vpc.igw_id
+    igw_id = module.tf_demo_vpc.igw_id // vpc internet gatway
 }
 
 module "public_subnet_b" {
     source = "./modules/public_subnet"
     name = "tf-demo-public-subnet-b"
     vpc_id = module.tf_demo_vpc.vpc_id
-    subnet_cidr_block = "10.0.1.0/24" // 251
+    subnet_cidr_block = "10.0.1.0/24" // 251 public addresses
     owner = var.owner
     environment = var.environment
     az = "${var.region}b"
-    igw_id = module.tf_demo_vpc.igw_id
+    igw_id = module.tf_demo_vpc.igw_id // vpc internat gatway
 }
 
 module "private_subnet_a" {
     source = "./modules/private_subnet"
     name = "tf-demo-private-subnet-a"
     vpc_id = module.tf_demo_vpc.vpc_id
-    subnet_cidr_block = "10.0.16.0/20" // 4091
+    subnet_cidr_block = "10.0.16.0/20" // 4091 private addresses
     owner = var.owner
     environment = var.environment
     az = "${var.region}a"
-    nat_gw_id = module.public_subnet_a.nat_gw_id
+    nat_gw_id = module.public_subnet_a.nat_gw_id // natgatway in public subnet
 }
 
 module "private_subnet_b" {
     source = "./modules/private_subnet"
     name = "tf-demo-private-subnet-b"
     vpc_id = module.tf_demo_vpc.vpc_id
-    subnet_cidr_block = "10.0.32.0/20" // 4091
+    subnet_cidr_block = "10.0.32.0/20" // 4091 private addresses
     owner = var.owner
     environment = var.environment
     az = "${var.region}b"
-    nat_gw_id = module.public_subnet_b.nat_gw_id
+    nat_gw_id = module.public_subnet_b.nat_gw_id // natgatway in public subnet
 }
